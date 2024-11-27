ce6a5f424e68154f50d11ec981c35465ff90ffbd
MaximLucifer <slenderman132456@gmail.com>
2024-11-27 10:24:02 +0300

    dodelano

--- kartochki/minishop/views.py
+++ kartochki/minishop/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Clothing, Electronics, Automobile, HomeAppliance, Jewelry
 
 # Create your views here.
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+    # Получаем все объекты из всех конкретных моделей
+    clothing_products = Clothing.objects.all()
+    electronics_products = Electronics.objects.all()
+    automobile_products = Automobile.objects.all()
+    home_appliance_products = HomeAppliance.objects.all()
+    jewelry_products = Jewelry.objects.all()
+
+    # Объединяем все продукты в один список (если необходимо)
+    products = list(clothing_products) + list(electronics_products) + list(automobile_products) + list(home_appliance_products) + list(jewelry_products)
+
+    return render(request, 'minishop/product_list.html', {'products': products})
