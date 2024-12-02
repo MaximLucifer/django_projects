066265f86cae5bd358c392d039c7668c37ebe0c6
MaximLucifer <slenderman132456@gmail.com>
2024-12-02 12:14:09 +0300

    ozerkova klining v.2

--- cleaning/cleaning/settings.py
+++ cleaning/cleaning/settings.py
@@ -105,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
--- cleaning/main/urls.py
+++ cleaning/main/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('booking/', views.booking, name='booking'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('profile/', views.profile, name='profile'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
